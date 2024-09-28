1f9d709c0df27f8ac79e064fd36e9fe33214129b
Andrey <andrejlapin4594@gmail.com>
2024-09-28 19:39:31 +0500

    рабочий чат

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'daphne',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Расположение основного asgi приложения в проекте
 ASGI_APPLICATION = "django_project.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
